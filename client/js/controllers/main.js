3329092da69fae1c59bb6b34d43392c3f07208df
kreloff <kreloff@wisc.edu>
2016-08-08 16:52:15 -0500

    began work on schedule section

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,5 +1,25 @@
 angular.module('sdc')
 
 .controller("mainController", function($scope, $http) {
-  $scope.welcome = "Hello! Angular test.";
 });
+
+(function () {
+  'use strict';
+
+  angular
+      .module('sdc')
+      .controller('ScheduleCtrl', ScheduleCtrl);
+
+  function ScheduleCtrl ( $scope ) {
+    $scope.data = {
+      selectedIndex: 0,
+    };
+    $scope.next = function() {
+      $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+    };
+    $scope.previous = function() {
+      $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+    };
+  }
+})();
+
