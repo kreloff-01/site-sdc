4a2eea0b2fa0438db0e723c3064854a38d81b819
kreloff <kreloff@wisc.edu>
2016-08-09 17:00:20 -0500

    more work on the calendar

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,4 +1,4 @@
-angular.module('sdc')
+var main = angular.module('sdc')
 
 .controller("mainController", function($scope, $http) {
 });
@@ -22,4 +22,51 @@ angular.module('sdc')
     };
   }
 })();
+
+/* current element */
+
+main.directive("testDir", function () {
+    function link(scope, element) {
+    }
+    return {
+        restrict: "AE",
+        link: link,
+        controller:function($scope,$element){
+            $scope.name2 = 'this is second name';
+            var barGridSection = $element.find('#barGridSection'); //helps to find the child element.
+            console.log("does this work?");
+        }
+
+    };
+})
+
+/* dynamic tile generation for calendar days */
+angular
+    .module('sdc')
+    .controller('gridListDemoCtrl', function($scope) {
+      this.tiles = buildGridModel({
+        background: ""
+      });
+        function buildGridModel(tileTmpl){
+            var it, results = [ ];
+            for (var j=0; j<11; j++) {
+                it = angular.extend({},tileTmpl);
+                it.title = (j+1);
+                it.span  = { row : 1, col : 1 };
+                it.background = "white";
+                results.push(it);
+            }
+            return results;
+        }
+
+      this.info = generateInfo({
+        day: "",
+        content: ""
+      });
+        function generateInfo(currTile){
+            var information = [ ];
+            
+        };
+
+    });
 
