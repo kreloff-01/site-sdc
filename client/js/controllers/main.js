419af42e6c892c098f8bd2c928d6ee7658b3231b
EK <kreloff@wisc.edu>
2016-08-28 20:01:27 -0500

    fucking scopes. if i get them to do my bidding, i will be DONE but they refuse to listen

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -22,7 +22,7 @@ var site = angular.module('sdc')
   }
 })();
 
-/* current element WIP*/
+/* current element WIP
 
 site.directive("testDir", function () {
     function link(scope, element) {
@@ -37,52 +37,67 @@ site.directive("testDir", function () {
         }
 
     };
-})
+}) */
+
+site.controller('tabGen', function($scope){
+    var filledDays, blankDay;
+    this.tabs = getTabsToGenerate({
+        days:"",
+        blankDays:"",
+        month:""
+    });
+    function getTabsToGenerate(tabs) {
+        var it, result = [], data = [];
+        data.push({month: "September", days: 30, blankDays: 4});
+        data.push({month: "October", days: 31, blankDays: 6});
+        data.push({month: "November", days: 30, blankDays: 2});
+        data.push({month: "December", days: 31, blankDays: 4});
+        for (var i = 0; i < 4; i++) {
+            it = angular.extend({}, tabs);
+            it.days = data[i].days;
+            it.month = data[i].month;
+            it.blankDays = data[i].blankDays;
+            result.push(it);
+        }
+        return result;
+    }
+});
 
 /* dynamic tile generation for calendar days -- WIP */
 site.controller('gridListCtrl', function($scope) {
-      this.tiles = buildGridModel({
+    $scope.init = function(days, blankDays){
+        $scope.normalDays = days;
+        $scope.emptyDays = blankDays;
+        console.log($scope.normalDays + " normal days init");
+        console.log($scope.blankDays + " blank days init");
+    };
+
+    this.tiles = buildGridModel({
         background: "",
         footer: ""
-      });
-        var month = '';
-        function buildGridModel(tileTmpl, month){
-            var it, results = [ ];
-            var days = 0;
-            var month = "Sept";
-            switch(month){
-                case "Sept":{
-                    days = 30;
-                    for (var j=0; j<4; j++) {
-                        it = angular.extend({},tileTmpl);
-                        it.title = "";
-                        it.span  = { row : 1, col : 1 };
-                        it.background = "blankClass";
-                        it.footer = "blankClass";
-                        results.push(it);
-                    }
-                }
-            }
-
-            for (var j=0; j<days; j++) {
-                it = angular.extend({},tileTmpl);
-                it.title = (j+1);
-                it.span  = { row : 1, col : 1 };
-                it.background = "tileBackground";
-                results.push(it);
-            }
-            return results;
+    });
+    function buildGridModel(tileTmpl){
+        var it,results = [ ];
+        console.log($scope.normalDays + " normal days func");
+        console.log($scope.blankDays + " blank days func");
+        for (var j=0; j<$scope.emptyDays; j++) {
+            it = angular.extend({},tileTmpl);
+            it.title = "";
+            it.span  = { row : 1, col : 1 };
+            it.background = "blankClass";
+            it.footer = "blankClass";
+            results.push(it);
         }
 
-      this.info = generateInfo({
-        day: "",
-        content: ""
-      });
-        function generateInfo(currTile){
-            var information = [ ];
-
-        };
-
+        for (var j=0; j<$scope.normalDays; j++) {
+            it = angular.extend({},tileTmpl);
+            it.title = (j+1);
+            it.span  = { row : 1, col : 1 };
+            it.background = "tileBackground";
+            results.push(it);
+        }
+        return results;
+    }
     });
 
 /* days of the week -- works */
